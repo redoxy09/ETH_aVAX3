97e310bdd75a2b03a3e378ceed863f4878ce1444
Redox <130912134+redoxy09@users.noreply.github.com>
2024-07-12 11:11:35 +0530

    Create Mtoken.sol

---
+++ Mtoken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract MToken is ERC20 {
+    address public owner;
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        owner = msg.sender;
+        _mint(msg.sender, 10);
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
